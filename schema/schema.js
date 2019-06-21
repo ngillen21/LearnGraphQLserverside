588c5d087c695579d3ebf3f18f046b0fa3d3c063
Nick Gillen <nick.gillen@d51schools.org>
2019-06-21 10:42:44 -0600

    removing comments

--- schema/schema.js
+++ schema/schema.js
@@ -37,7 +37,6 @@ const AuthorType = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             resolve(parent,args){
-                // return _.filter(books,{authorId: parent.id});
                 return Book.find({authorId: parent.id});
             }
         }
@@ -51,8 +50,6 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: { id:{ type:GraphQLID } },
             resolve(parent, args){
-                //code to get data from db / other source
-                // return _.find(books,{id:args.id});
                 return Book.findById(args.id);
             }
         },
@@ -60,21 +57,18 @@ const RootQuery = new GraphQLObjectType({
             type: AuthorType,
             args: { id: {type: GraphQLID } },
             resolve(parent, args){
-                // return _.find(authors,{id:args.id});
                 return Author.findById(args.id);
             }
         },
         books:{
             type: new GraphQLList(BookType),
             resolve(parent, args){
-                // return books
                 return Book.find({});
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args){
-                // return authors
                 return Author.find({});
             }
         }
